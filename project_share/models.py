ce09a1014cb8466151e768b3d3f1a80f2f7951ff
SillyInventor <SillyInventor@gmail.com>
2017-04-07 16:53:31 -0400

    Bootstrap templates & started project_share Linted proj../models Linted proj../urls

--- project_share/models.py
+++ project_share/models.py
@@ -4,39 +4,48 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.template.defaultfilters import slugify
 from django_comments.moderation import CommentModerator, moderator
-from taggit.models import TaggedItemBase, GenericTaggedItemBase
-from django_teams.models import Team
-
-import secretballot
-import json
-import zipfile as zippy
-
 from taggit.managers import TaggableManager
+
 
 def application_application(instance, filename):
     return "applications/" + slugify(instance.name) + "." + slugify(filename.split('.')[-1])
 
+
 def application_application_demo(instance, filename):
-    return "applications/demos/" + slugify(instance.application.__unicode__()) + "/" + slugify(filename.split('.')[:-1]) + "." + slugify(filename.split('.')[-1])
+    return "applications/demos/" + slugify(instance.application.__unicode__())\
+           + "/" + slugify(filename.split('.')[:-1]) + "." + slugify(filename.split('.')[-1])
+
 
 def application_application_goal(instance, filename):
-    return "applications/goals/" + slugify(instance.application.__unicode__()) + "/" + slugify(filename.split('.')[:-1]) + "." + slugify(filename.split('.')[-1])
+    return "applications/goals/" + slugify(instance.application.__unicode__())\
+           + "/" + slugify(filename.split('.')[:-1]) + "." + slugify(filename.split('.')[-1])
+
 
 def application_library(instance, filename):
-    return "applications/libraries/" + slugify(filename.split('.')[:-1]) + "." + slugify(filename.split('.')[-1])
+    return "applications/libraries/" + slugify(filename.split('.')[:-1])\
+           + "." + slugify(filename.split('.')[-1])
+
 
 def project_project(instance, filename):
-    return "applications/files/" + slugify(instance.owner.__unicode__() + '/' + '.'.join(filename.split('.')[:-1])) + "." + slugify(filename.split('.')[-1])
+    return "applications/files/" + slugify(instance.owner.__unicode__()
+                                           + '/' + '.'.join(filename.split('.')[:-1]))\
+                                           + "." + slugify(filename.split('.')[-1])
+
 
 def project_screenshot(instance, filename):
-    return "applications/screenshots/" + slugify(instance.owner.__unicode__() + '/' + '.'.join(filename.split('.')[:-1])) + "." + slugify(filename.split('.')[-1])
+    return "applications/screenshots/" + slugify(instance.owner.__unicode__()
+                                                 + '/' + '.'.join(filename.split('.')[:-1]))\
+                                                 + "." + slugify(filename.split('.')[-1])
+
 
 # These need to be removed someday; not removed now as it causes an error message
 def module_module(instance, filename):
     return "modules/" + slugify(instance.name) + "." + slugify(filename.split('.')[-1])
 
+
 def module_library(instance, filename):
     return "modules/libraries/" + instance.name
+
 
 class Classroom(models.Model):
     name = models.CharField(max_length=255)
@@ -46,6 +55,7 @@ class Classroom(models.Model):
     def __unicode__(self):
         return "%s's %s classroom" % (self.teacher, self.name)
 
+
 class Approval(models.Model):
     project = models.OneToOneField('Project')
     when_requested = models.DateTimeField(auto_now_add=True, null=True, blank=True)
@@ -54,6 +64,7 @@ class Approval(models.Model):
 
     def __unicode__(self):
         return "%s approval for %s" % (self.project.owner, self.project)
+
 
 class Application(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -66,7 +77,7 @@ class Application(models.Model):
 
     more_info_url = models.URLField(null=True, blank=True)
 
-    application_type = models.CharField(max_length=5, choices= (
+    application_type = models.CharField(max_length=5, choices=(
             ('CSNAP', 'cSnap'),
             ('BLOCK', 'Blockly/Scratch'),
         ))
@@ -97,6 +108,7 @@ class Application(models.Model):
     def __unicode__(self):
         return self.name
 
+
 class ApplicationContext(models.Model):
     application = models.ForeignKey('project_share.Application')
     parent = models.ForeignKey('project_share.ApplicationContext', null=True, blank=True)
@@ -106,12 +118,13 @@ class ApplicationContext(models.Model):
     html_data = models.TextField(null=True, blank=True)
 
     def level(self):
-        if self.parent != None:
+        if self.parent is not None:
             return self.parent.level()+1
         return 1
 
     def __unicode__(self):
         return self.application.__unicode__() + "/" + self.title
+
 
 class ApplicationDemo(models.Model):
     name = models.CharField(max_length=255)
@@ -154,6 +167,7 @@ class Project(models.Model):
     def get_absolute_url(self):
         return reverse('project-detail', kwargs={'pk': self.pk})
 
+
 class Goal(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
@@ -165,33 +179,40 @@ class Goal(models.Model):
     def __unicode__(self):
         return self.name
 
+
 class ExtendedUser(AbstractUser):
     gender = models.CharField(max_length=100, null=True, blank=True)
     race = models.CharField(max_length=100, null=True, blank=True)
     age = models.PositiveIntegerField(null=True, blank=True)
+
     def __unicode__(self):
         if self.first_name != "":
             return "%s %s" % (self.first_name, self.last_name)
         return self.username
 
+
 class FileUpload(models.Model):
     f = models.FileField(upload_to='files/%Y-%m-%d/')
+
 
 class ProjectModerator(CommentModerator):
     moderate_after = -1
 
+
 moderator.register(Project, ProjectModerator)
+
 
 class Address(models.Model):
     school = models.CharField(max_length=255)
     town = models.CharField(max_length=255)
-    state = models.CharField(max_length=255, verbose_name = 'State or Province')
+    state = models.CharField(max_length=255, verbose_name='State or Province')
     country = models.CharField(max_length=255)
     phone = models.CharField(max_length=255)
     teacher = models.OneToOneField(settings.AUTH_USER_MODEL, primary_key=True)
 
     class Meta:
-      verbose_name_plural = "Addresses"
+        verbose_name_plural = "Addresses"
+
 
 class ApplicationTheme(models.Model):
     name = models.CharField(max_length=255)
@@ -199,6 +220,7 @@ class ApplicationTheme(models.Model):
 
     def __unicode__(self):
         return self.name
+
 
 class ApplicationCategory(models.Model):
     theme = models.ForeignKey('project_share.ApplicationTheme')
@@ -208,5 +230,3 @@ class ApplicationCategory(models.Model):
 
     def __unicode__(self):
         return self.name
-
-import project_share.signals
--- project_share/urls.py
+++ project_share/urls.py
@@ -20,22 +20,28 @@ urlpatterns = [
     url(r'^projects/(?P<pk>\d+)/edit$', ProjectUpdate.as_view(), name='project-update'),
     url(r'^projects/(?P<pk>\d+)/delete$', ProjectDelete.as_view(), name='project-delete'),
     url(r'^projects/(?P<pk>\d+)/unpublish$', ProjectUnpublish.as_view(), name='project-unpublish'),
-    url(r'^projects/delete/success$', TemplateView.as_view(template_name='project_share/project_delete_success.html'), name='project-delete-success'),
-    url(r'^projects/unpublish/success$', TemplateView.as_view(template_name='project_share/project_unpublish_success.html'), name='project-unpublish-success'),
+    url(r'^projects/delete/success$',
+        TemplateView.as_view(template_name='project_share/project_delete_success.html'),
+        name='project-delete-success'),
+    url(r'^projects/unpublish/success$',
+        TemplateView.as_view(template_name='project_share/project_unpublish_success.html'),
+        name='project-unpublish-success'),
 
     url(r'projects/(?P<project_pk>\d+)/publish$', ApprovalCreate.as_view(), name='approval-create'),
-    url(r'approval/confirm$', TemplateView.as_view(template_name='project_share/approval_confirm.html'), name='approval-confirm'),
+    url(r'approval/confirm$', TemplateView.as_view(template_name='project_share/approval_confirm.html'),
+        name='approval-confirm'),
 
     url(r'applications/$', ApplicationList.as_view(), name='application-list'),
     url(r'applications/(?P<pk>\d+)$', ApplicationDetail.as_view(), name='application-detail'),
     url(r'applications/(?P<pk>\d+)/run$', ApplicationRunDetail.as_view(), name='application-run-detail'),
-    
+
     url(r'^demos/$', DemoList.as_view(), name='demo-list'),
     url(r'^demos/(?P<pk>\d+)$', DemoDetail.as_view(), name='demo-detail'),
 
     url(r'users/(?P<pk>\d+)$', UserDetail.as_view(), name='extendeduser-detail'),
-    
+
     url(r'address/create$', AddressCreate.as_view(), name='address-create'),
-    url(r'address/confirm$', TemplateView.as_view(template_name='project_share/address_confirm.html'), name='address-confirm'),
+    url(r'address/confirm$', TemplateView.as_view(template_name='project_share/address_confirm.html'),
+        name='address-confirm'),
     url(r'address/(?P<pk>.*)$', AddressUpdate.as_view(), name='address-update'),
 ]
