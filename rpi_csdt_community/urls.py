8bed9cd4edd3cbe70537b65883caff888064b0ed
SillyInventor <SillyInventor@gmail.com>
2017-04-07 15:35:32 -0400

    Initial Linting Add flake config for line length = 119 per Django rules and ignoring migrations Add to travis -- prepare for failed builds Lint manage.py Lint rpi.../urls.py

--- rpi_csdt_community/urls.py
+++ rpi_csdt_community/urls.py
@@ -1,17 +1,16 @@
 from django.conf.urls import include, url
-from django.views.generic import TemplateView, RedirectView
-from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 from django.conf import settings
-import debug_toolbar
-
-from django.contrib import admin
-admin.autodiscover()
-
-from rpi_csdt_community.viewsets import ProjectViewSet, DemosViewSet, GoalViewSet, ApplicationViewSet, FileUploadView, CurrentUserView, TeamViewSet
-from rpi_csdt_community.viewsets import ApplicationThemeViewSet, ApplicationCategoryViewSet
+from rpi_csdt_community.viewsets import ProjectViewSet, DemosViewSet, GoalViewSet, ApplicationViewSet, FileUploadView
+from rpi_csdt_community.viewsets import CurrentUserView, TeamViewSet, ApplicationThemeViewSet
+from rpi_csdt_community.viewsets import ApplicationCategoryViewSet
 from rpi_csdt_community.views import home
 from django.views import static
 from rest_framework import routers
+from django.contrib import admin
+import debug_toolbar
+admin.autodiscover()
+
 router = routers.DefaultRouter()
 router.register(r'projects', ProjectViewSet, base_name='api-projects')
 router.register(r'team', TeamViewSet, base_name='api-teams')
@@ -21,14 +20,13 @@ router.register(r'application', ApplicationViewSet, base_name='api-modules')
 router.register(r'theme', ApplicationThemeViewSet, base_name='api-themes')
 router.register(r'category', ApplicationCategoryViewSet, base_name='api-category')
 
-		
+
 urlpatterns = [
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
     # TemplateView + Login
     url(r'^$', home, {}, 'home'),
-    #url(r'^$', login_required(TemplateView.as_view(template_name="home.html")), {}, 'home'),
 
     url(r'', include('project_share.urls')),
     url(r'teams/', include('django_teams.urls')),
@@ -49,8 +47,8 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': settings.MEDIA_ROOT,}),
-    url(r'^static/(?P<path>.*)$', static.serve, {'document_root': settings.STATIC_ROOT,}),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', static.serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.ENABLE_GIS:
