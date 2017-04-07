86a0d17dde8f880db8c93aa4ff4738f964e9a0fa
SillyInventor <SillyInventor@gmail.com>
2017-04-07 18:24:10 -0400

    Finished flake linting Linted proj../templatetags/porkject_share_tags

--- project_share/templatetags/project_share_tags.py
+++ project_share/templatetags/project_share_tags.py
@@ -1,10 +1,8 @@
 from django import template
-from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
 from project_share.models import Project
-
-from django.conf import settings
 register = template.Library()
+
 
 def unrestricted_projects(user, requester):
     try:
@@ -12,6 +10,7 @@ def unrestricted_projects(user, requester):
     except:
         o = Project.objects.filter(Q(owner=user), Q(approved=True))
     return o.all()
+
 
 register = template.Library()
 register.filter('unrestricted_projects', unrestricted_projects)
