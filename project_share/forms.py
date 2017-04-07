6054c67e422f255bfca5f54f51c2075bc044fb4b
SillyInventor <SillyInventor@gmail.com>
2017-04-07 18:04:50 -0400

    Most of The Way Through project_share Linted proj../forms Linted proj../views

--- project_share/forms.py
+++ project_share/forms.py
@@ -1,34 +1,44 @@
 from django import forms
 from django.forms import ModelForm
 
-from project_share.models import Project, Approval, Address, Application, ApplicationCategory, ExtendedUser
+from project_share.models import Project, Approval, Address, Application, ApplicationCategory
 from django_teams.models import Team, TeamStatus
 
+
 class ProjectForm(ModelForm):
-    def __init__(self,*args,**kwargs):
+    def __init__(self, *args, **kwargs):
         user = kwargs.pop('user')
-        super (ProjectForm,self ).__init__(*args,**kwargs) # populates the post
-        self.fields['classroom'].queryset = Team.objects.filter(users=user) # make sure we're only getting the right classrooms
+        super(ProjectForm, self).__init__(*args, **kwargs)  # populates the post
+        # make sure we're only getting the right classrooms
+        self.fields['classroom'].queryset = Team.objects.filter(users=user)
 
     class Meta:
         model = Project
-        exclude = ('owner', 'approved','application','project','screenshot','parent',)
-		
+        exclude = ('owner', 'approved', 'application', 'project', 'screenshot', 'parent')
+
+
 class ProjectUnpublishForm(ModelForm):
     class Meta:
         model = Project
-        exclude = ('owner', 'approved','application','project','screenshot','parent',)
+        exclude = ('owner', 'approved', 'application', 'project', 'screenshot', 'parent')
+
 
 class ApprovalForm(ModelForm):
     class Meta:
         model = Approval
-        exclude = ('project', 'when_requested', 'when_updated', 'approved_by',)
-		
+        exclude = ('project', 'when_requested', 'when_updated', 'approved_by')
+
+
 class ExtendedSignupForm(forms.Form):
-    gender = forms.CharField(max_length=100, label='gender', widget=forms.TextInput(attrs={'placeholder': 'gender (optional)'}), required=False)
-    race = forms.CharField(max_length=100, label='race', widget=forms.TextInput(attrs={'placeholder': 'race (optional)'}), required=False)
-    age = forms.IntegerField(label='age', widget=forms.TextInput(attrs={'placeholder': 'age (optional)'}), required=False)
-    classroom = forms.IntegerField(label='classroom', widget=forms.TextInput(attrs={'placeholder': 'classroom # (optional)'}), required=False)
+    gender = forms.CharField(max_length=100, label='gender',
+                             widget=forms.TextInput(attrs={'placeholder': 'gender (optional)'}), required=False)
+    race = forms.CharField(max_length=100, label='race',
+                           widget=forms.TextInput(attrs={'placeholder': 'race (optional)'}), required=False)
+    age = forms.IntegerField(label='age', widget=forms.TextInput(attrs={'placeholder': 'age (optional)'}),
+                             required=False)
+    classroom = forms.IntegerField(label='classroom',
+                                   widget=forms.TextInput(attrs={'placeholder': 'classroom # (optional)'}),
+                                   required=False)
     field_order = ['username', 'email', 'password1', 'password2', 'gender', 'race', 'age', 'classroom']
 
     def signup(self, request, n_user):
@@ -37,19 +47,22 @@ class ExtendedSignupForm(forms.Form):
         n_user.age = self.cleaned_data['age']
         n_user.save()
         team = self.cleaned_data['classroom']
-        if not team is None:
-           TeamStatus(team=Team.objects.get(pk=team), role = 1, user = n_user, comment = 'just signed up').save()
+        if team is not None:
+            TeamStatus(team=Team.objects.get(pk=team), role=1, user=n_user, comment='just signed up').save()
+
 
 class AddressForm(ModelForm):
     class Meta:
         model = Address
         exclude = ('teacher',)
 
+
 class ApplicationAdminForm(ModelForm):
     categories = forms.ModelMultipleChoiceField(label='Categories',
-        queryset=ApplicationCategory.objects.all(), required=False,
-        help_text='Select which categories this application belongs too; think about multiple themes (computing, math, cultural)')
-
+                                                queryset=ApplicationCategory.objects.all(), required=False,
+                                                help_text='Select which categories this application belongs too; \
+                                                think about multiple themes (computing, \
+                                                math, cultural)')
 
     class Meta:
         model = Application
--- project_share/views.py
+++ project_share/views.py
@@ -13,16 +13,17 @@ from taggit.models import Tag
 from extra_views import SortableListMixin
 from extra_views import SearchableListMixin
 
-from project_share.models import Application, Project, ApplicationDemo, Classroom, Approval, Address
+from project_share.models import Application, Project, ApplicationDemo, Approval, Address
 from project_share.forms import ProjectForm, ApprovalForm, AddressForm, ProjectUnpublishForm
 from django_teams.models import Ownership
 
 try:
     from django.contrib.auth import get_user_model
-except ImportError: # django < 1.5
+except ImportError:  # django < 1.5
     from django.contrib.auth.models import User
 else:
     User = get_user_model()
+
 
 class RestrictPermissionMixin(object):
     def dispatch(self, request, *args, **kwargs):
@@ -41,11 +42,14 @@ class RestrictPermissionMixin(object):
         self.object = o
         return o
 
+
 class ApplicationList(ListView):
     model = Application
 
+
 class ApplicationDetail(DetailView):
     model = Application
+
 
 class ApplicationRunDetail(DetailView):
     model = Application
@@ -53,48 +57,52 @@ class ApplicationRunDetail(DetailView):
 
     def render_to_response(self, context, **response_kwargs):
         try:
-           context['GOOGLE_ANALYTICS_PROPERTY_ID'] = settings.GOOGLE_ANALYTICS_PROPERTY_ID
+            context['GOOGLE_ANALYTICS_PROPERTY_ID'] = settings.GOOGLE_ANALYTICS_PROPERTY_ID
         except:
-           pass
+            pass
         return super(ApplicationRunDetail, self).render_to_response(context, **response_kwargs)
-    
+
     def get_template_names(self):
         return ['project_share/application_%s.html' % self.object.application_type.lower()]
 
+
 class ProjectList(SearchableListMixin, SortableListMixin, ListView):
     sort_fields_aliases = [('name', 'by_name'), ('id', 'by_id'), ('votes', 'by_likes'), ]
-    search_fields = [('application__name','iexact')]
+    search_fields = [('application__name', 'iexact')]
     search_split = False
     model = Project
     paginate_by = 20
     ordering = ["-when"]
-	
+
     def get_queryset(self):
         set = Project.approved_projects()
         filter_val = self.request.GET.get('filter')
         if filter_val is not None:
-           set = set.filter(application=filter_val,)
+            set = set.filter(application=filter_val,)
         order = self.request.GET.get('orderby')
         if order is not None:
-           set = set.order_by(order)
+            set = set.order_by(order)
         return set
 
     def render_to_response(self, context, **response_kwargs):
         context['application_list'] = Application.objects.all()
         return super(ProjectList, self).render_to_response(context, **response_kwargs)
 
+
 class ProjectTagList(ProjectList):
     def get_queryset(self):
         self.tag = get_object_or_404(Tag, pk=self.kwargs['tag_pk'])
         return Project.approved_projects().filter(tags__in=[self.tag])
 
+
 class ProjectDetail(RestrictPermissionMixin, DetailView):
     queryset = Project.objects.select_related("approval").select_related("owner").select_related("screenshot")
-	
+
     def render_to_response(self, context, **response_kwargs):
         o = self.get_object()
         context['hasApproval'] = (hasattr(o, 'approval') or o.approved)
         return super(ProjectDetail, self).render_to_response(context, **response_kwargs)
+
 
 class ProjectRunDetail(RestrictPermissionMixin, DetailView):
     model = Project
@@ -104,15 +112,17 @@ class ProjectRunDetail(RestrictPermissionMixin, DetailView):
     def render_to_response(self, context, **response_kwargs):
         context['application'] = context['project'].application
         try:
-           context['GOOGLE_ANALYTICS_PROPERTY_ID'] = settings.GOOGLE_ANALYTICS_PROPERTY_ID
+            context['GOOGLE_ANALYTICS_PROPERTY_ID'] = settings.GOOGLE_ANALYTICS_PROPERTY_ID
         except:
-           pass
+            pass
         return super(ProjectRunDetail, self).render_to_response(context, **response_kwargs)
+
 
 class ProjectPresentDetail(ProjectRunDetail):
     def render_to_response(self, context, **response_kwargs):
         context['present'] = True
         return super(ProjectPresentDetail, self).render_to_response(context, **response_kwargs)
+
 
 class ProjectCreate(CreateView):
     model = Project
@@ -129,6 +139,7 @@ class ProjectCreate(CreateView):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super(ProjectCreate, self).form_valid(form)
+
 
 class ProjectUpdate(UpdateView):
     model = Project
@@ -162,6 +173,7 @@ class ProjectUpdate(UpdateView):
             raise PermissionDenied()
         return o
 
+
 class ProjectDelete(DeleteView):
     model = Project
     success_url = reverse_lazy('project-delete-success')
@@ -171,12 +183,13 @@ class ProjectDelete(DeleteView):
         if not o.owner == self.request.user:
             raise PermissionDenied('this isn\'t your project')
         return o
-	
+
+
 class ProjectUnpublish(UpdateView):
     model = Project
     template_name = "project_share/project_unpublish.html"
     form_class = ProjectUnpublishForm
-	
+
     def post(self, request, *args, **kwargs):
         if 'Unpublish' in request.POST:
             proj = super(ProjectUnpublish, self).get_object()
@@ -190,7 +203,8 @@ class ProjectUnpublish(UpdateView):
                 pass
 
             try:
-                ownership = Ownership.objects.filter(content_type_id=ContentType.objects.get_for_model(proj)).get(object_id=proj.id)
+                ownership = Ownership.objects.filter(content_type_id=ContentType.objects.get_for_model(proj)).get(
+                    object_id=proj.id)
                 ownership.delete()
             except:
                 pass
@@ -209,46 +223,42 @@ class ProjectUnpublish(UpdateView):
         if o.owner != self.request.user:
             raise PermissionDenied('this isn\'t your project')
         return o
-    def get_object(self):
-        o = super(ProjectUnpublish, self).get_object()
 
-        # If the object doesn't belong to this user, throw a error 503
-        if o.owner != self.request.user:
-            raise PermissionDenied('this isn\'t your project')
-        return o
 
 class DemoList(ListView):
     model = ApplicationDemo
 
-class DemoDetail(DetailView):		
+
+class DemoDetail(DetailView):
     model = ApplicationDemo
     context_object_name = 'applicationdemo'
     template_name = "project_share/application_csnap.html"
 
     def render_to_response(self, context, **response_kwargs):
         try:
-           context['GOOGLE_ANALYTICS_PROPERTY_ID'] = settings.GOOGLE_ANALYTICS_PROPERTY_ID
+            context['GOOGLE_ANALYTICS_PROPERTY_ID'] = settings.GOOGLE_ANALYTICS_PROPERTY_ID
         except:
-           pass
+            pass
         return super(DemoDetail, self).render_to_response(context, **response_kwargs)
+
 
 class ApprovalCreate(CreateView):
     model = Approval
     form_class = ApprovalForm
 
     def dispatch(self, request, *args, **kwargs):
-      self.kwargs = kwargs
-      self.request = request
-      return super(ApprovalCreate, self).dispatch(request, *args, **kwargs)
+        self.kwargs = kwargs
+        self.request = request
+        return super(ApprovalCreate, self).dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-      
+
         project_id = self.kwargs['project_pk']
-      
-        team_approval = Ownership() # Create an ownership object
+
+        team_approval = Ownership()  # Create an ownership object
         team_approval.content_type = ContentType.objects.get(app_label="project_share", model="project")
         team_approval.object_id = project_id
-        if Project.objects.get(pk=project_id).classroom == None:
+        if Project.objects.get(pk=project_id).classroom is None:
             team_approval = None
             return super(ApprovalCreate, self).post(request, *args, **kwargs)
         team_approval.team = Project.objects.get(pk=project_id).classroom
@@ -256,7 +266,7 @@ class ApprovalCreate(CreateView):
         team_approval.save()
 
         return super(ApprovalCreate, self).post(request, *args, **kwargs)
-      
+
     def form_valid(self, form):
         form.instance.project_id = self.kwargs['project_pk']
         return super(ApprovalCreate, self).form_valid(form)
@@ -268,10 +278,11 @@ class ApprovalCreate(CreateView):
 class UserDetail(DetailView):
     model = User
     template_name = "project_share/user_detail.html"
-	
+
     def render_to_response(self, context, **response_kwargs):
         context['user'] = self.request.user
         return super(UserDetail, self).render_to_response(context, **response_kwargs)
+
 
 class AddressCreate(CreateView):
     model = Address
@@ -284,6 +295,7 @@ class AddressCreate(CreateView):
     def get_success_url(self):
         return u'/'
 
+
 class AddressUpdate(UpdateView):
     model = Address
     form_class = AddressForm
@@ -291,9 +303,9 @@ class AddressUpdate(UpdateView):
     template_name = "project_share/address_form.html"
 
     def dispatch(self, request, *args, **kwargs):
-      if(not request.user.id == int(self.kwargs['pk'])):
-         raise PermissionDenied
-      return super(AddressUpdate, self).dispatch(request, *args, **kwargs)
+        if(not request.user.id == int(self.kwargs['pk'])):
+            raise PermissionDenied
+        return super(AddressUpdate, self).dispatch(request, *args, **kwargs)
 
     def get_success_url(self):
         return reverse('address-confirm')
