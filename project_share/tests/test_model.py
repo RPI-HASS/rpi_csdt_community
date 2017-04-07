de3175defd684017be2e38897701894e530791d4
SillyInventor <SillyInventor@gmail.com>
2017-04-07 18:21:38 -0400

    Second to Last Flake commit (I hope) Linted test_api Linted test_model

--- project_share/tests/test_model.py
+++ project_share/tests/test_model.py
@@ -1,14 +1,11 @@
 from django.test import TestCase
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.test.client import Client
+from project_share.models import Project
 
-from project_share.models import *
 
 class ProjectTests(TestCase):
     fixtures = ['default.json']
 
     def test_only_shows_published_projects(self):
         projects = Project.approved_projects().all()
-
         for project in projects:
             self.assertTrue(project.approved)
