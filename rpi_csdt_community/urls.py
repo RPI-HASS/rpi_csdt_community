d30dc1d0b05c7f22df0253dceb4b67587fa182e7
holmr <holmr@rpi.edu>
2017-06-02 10:37:24 -0400

    flake8

--- rpi_csdt_community/urls.py
+++ rpi_csdt_community/urls.py
@@ -62,5 +62,5 @@ urlpatterns += [
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': settings.MEDIA_ROOT, }),
     url(r'^static/(?P<path>.*)$', static.serve, {'document_root': settings.STATIC_ROOT, }),
     url(r'^culture/(?P<path>.*)$', static.serve, {'document_root':
-                                          settings.STATIC_ROOT + "website/www/", }),
+                                                  settings.STATIC_ROOT + "website/www/", }),
 ]
