54eb7f801e675c207e00c30870f1ff958615fdfe
holmr <holmr@rpi.edu>
2017-06-12 13:44:26 -0400

    Fixing requested items

--- blogposts/admin.py
+++ blogposts/admin.py
@@ -7,7 +7,6 @@ from .models import Post
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ["title", "updated", "timestamp"]
     list_display_links = ["updated"]
-    list_editable = ["title"]
     list_filter = ["updated", "timestamp"]
 
     search_fields = ["title", "content"]
