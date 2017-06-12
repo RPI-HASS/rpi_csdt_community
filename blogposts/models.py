330e79b55367823bc5244931366f2b09569443cc
holmr <holmr@rpi.edu>
2017-06-12 10:05:54 -0400

    Working on NoneType error

--- blogposts/models.py
+++ blogposts/models.py
@@ -31,7 +31,10 @@ def upload_location(instance, filename):
     # filebase, extension = filename.split(".")
     # return "%s/%s.%s" %(instance.id, instance.id, extension)
     PostModel = instance.__class__
-    new_id = PostModel.objects.order_by("id").last().id + 1
+    try:
+        new_id = PostModel.objects.order_by("id").last().id + 1
+    except:
+        new_id = 1
     """
     instance.__class__ gets the model Post. We must use this method because the model is defined below.
     Then create a queryset ordered by the "id"s of each object,
