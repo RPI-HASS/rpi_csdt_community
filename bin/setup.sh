a98e7d3610164a5308d0c9799299c2ff3fc6b863
Ruofan Liu <ruofanliu94@gmail.com>
2017-06-25 12:39:26 +0800

    it worked

--- bin/setup.sh
+++ bin/setup.sh
@@ -32,12 +32,6 @@ CREATE EXTENSION postgis_topology;
 cd /vagrant/
 #/////////////////
 
-=======
-echo 'rpi_csdt_community; CREATE EXTENSION adminpack; CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;' | psql -U postgres
-
-#echo "GOOGLE_API_KEY = "rgsehsfgsdfbsfdgxvfx532dfe52"" > /usr/local/lib/python2.7/dist-packages/gis_csdt/settings.py
-#echo "CENSUS_API_KEY = "gfsgerultiahefb4u9834tkbrfsb"" > /usr/local/lib/python2.7/dist-packages/gis_csdt/settings.py
->>>>>>> d983c3340b8dc0542511fcd7d6175d5f3e72b035
 # Install node
 sudo apt-get install -y nodejs build-essential
 
