db9aa108ea1ee8246087232bb1fb27f2d40b7bda
holmr <holmr@rpi.edu>
2017-06-26 09:59:52 -0400

    Flake8

--- rpi_csdt_community/urls.py
+++ rpi_csdt_community/urls.py
@@ -65,5 +65,5 @@ urlpatterns += [
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': settings.MEDIA_ROOT, }),
     url(r'^static/(?P<path>.*)$', static.serve, {'document_root': settings.STATIC_ROOT, }),
     url(r'^culture/(?P<path>.*)$', static.serve, {'document_root':
-                                                  settings.STATIC_WEBSITE_ROOT }),
+                                                  settings.STATIC_WEBSITE_ROOT}),
 ]
