39979666434974e1e9867c86975fe0b6b51b929c
holmr <holmr@rpi.edu>
2017-07-25 11:05:01 -0400

    Defaulting display_name to be username

--- project_share/models.py
+++ project_share/models.py
@@ -302,6 +302,8 @@ class ExtendedUser(AbstractUser):
         return "{} (@{})".format(self.display_name, self.username)
 
     def save(self, *args, **kwargs):
+        if not self.display_name:
+            self.display_name = self.username
         super(ExtendedUser, self).save(*args, **kwargs)
         if self.avatar:
             imglib.resize_image(self.avatar)
