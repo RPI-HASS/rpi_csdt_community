a7dae51b95dc7807ac4c946c3319738a70ce977b
holmr <holmr@rpi.edu>
2017-07-25 14:31:49 -0400

    200x200 img

--- project_share/imglib.py
+++ project_share/imglib.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
 
-def resize_image(avatar, size=(48, 48)):
+def resize_image(avatar, size=(200, 200)):
     image = Image.open(avatar.path)
     image.resize(size, Image.ANTIALIAS).save(avatar.path, 'PNG', quality=75)
