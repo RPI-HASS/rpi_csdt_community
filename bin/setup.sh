01c5e5733fe26ee14cabfc20c2c31c3f73f5db22
Ruofan Liu <ruofanliu94@gmail.com>
2017-07-31 11:37:26 +0800

    test

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,6 +5,8 @@ curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 rm nodesource_setup.sh
 
+sudo apt-get install binutils libproj-dev gdal-bin
+
 sudo apt-get update
 sudo apt-get upgrade
 
