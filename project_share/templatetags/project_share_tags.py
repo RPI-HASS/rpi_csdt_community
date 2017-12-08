000b8af48d1955229063d814a9c8068cf3dae232
Silly Inventor <SillyInventor@users.noreply.github.com>
2017-12-08 16:06:55 -0500

    This should fix all our loading woahs for the SPAs (#244)
    * This should fix all our loading woahs for the SPAs
    
    * Whoops, forgot to switch from the static URL I was testing
    
    * Ugh, and flake8... I must be tired
    
    * Removed base

--- project_share/templatetags/project_share_tags.py
+++ project_share/templatetags/project_share_tags.py
@@ -1,7 +1,8 @@
 from django import template
 from django.db.models import Q
-
 from project_share.models import Project
+from django.contrib.staticfiles import finders
+register = template.Library()
 
 
 def unrestricted_projects(user, requester):
@@ -17,6 +18,13 @@ def get_ownership_object(ownership):
     return ct.get_object_for_this_type(pk=ownership.object_id)
 
 
-register = template.Library()
+@register.simple_tag
+def includestatic(path, encoding='UTF-8'):
+    file_path = finders.find(path)
+    with open(file_path, "r") as f:
+        string = f.read()
+        return string
+
+
 register.filter('unrestricted_projects', unrestricted_projects)
 register.filter('get_ownership_object', get_ownership_object)
