89476eef61419d1a804297247da608c038d7d27f
serenasyw <yawen.shen620@gmail.com>
2018-02-20 16:38:48 -0500

    add tests for blogposts

--- blogposts/tests.py
+++ blogposts/tests.py
@@ -3,6 +3,7 @@ from django.test import LiveServerTestCase
 from rest_framework.test import APIClient
 from blogposts.models import Post
 from comments.models import Comment
+from blogposts.views import get_calendar
 
 User = get_user_model()
 
@@ -54,6 +55,21 @@ class tests(LiveServerTestCase):
         response = self.client.get(url, **{'HTTP_REFERER': url})
         self.assertTrue(response.status_code == 300 or response.status_code == 200,
                         msg="Got code %s on %s" % (response.status_code, url))
-        response = self.client.post(url, response.content, content_type="application/x-www-form-urlencoded")
-        self.assertTrue(response.status_code == 302 or response.status_code == 200,
+
+    def test_blog_views(self):
+        self.user = User.objects.get(username="test")
+        self.assertTrue(self.client.login(username='test', password='test'))
+        self.post = Post.objects.create(user=self.user, title="example", slug="example", content="example",
+                                        publish="2018-10-08")
+        self.assertTrue(self.client.login(username='test', password='test'))
+        url = '/news/example/'
+        self.client.logout()
+        response = self.client.get(url, **{'HTTP_REFERER': url})
+        self.assertTrue(response.status_code == 404,
                         msg="Got code %s on %s" % (response.status_code, url))
+
+        event_calendar = get_calendar([self.post])
+        self.assertTrue(len(event_calendar) == 1, 
+            msg="Got length %d on get_calendar" %(len(event_calendar)))
+
+
