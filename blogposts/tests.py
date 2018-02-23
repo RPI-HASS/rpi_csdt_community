471e142104966fd61e6ff6169ac88acc93a101af
serenasyw <yawen.shen620@gmail.com>
2018-02-22 22:04:56 -0500

    errors solved

--- blogposts/tests.py
+++ blogposts/tests.py
@@ -51,7 +51,8 @@ class tests(LiveServerTestCase):
         response = self.client.get(url, **{'HTTP_REFERER': url})
         self.assertTrue(response.status_code == 300 or response.status_code == 200,
                         msg="Got code %s on %s" % (response.status_code, url))
-        response = self.client.post(url, {'content_type': 'post', 'object_id': 1,'content': 'hello'})
+        response = self.client.post(url, {'content_type': 'post', 'object_id': 1,
+                                          'content': 'hello'})
         self.assertTrue(response.status_code == 302 or response.status_code == 200,
                         msg="Got code %s on %s" % (response.status_code, url))
         Comment.objects.create(user=self.user, content_type=self.post.get_content_type, object_id=1, content="example")
@@ -79,33 +80,29 @@ class tests(LiveServerTestCase):
         self.assertTrue(response.status_code == 302 or response.status_code == 200,
                         msg="Got code %s on %s" % (response.status_code, url))
 
-    def test_blog_forms(self):
-        form = PostForm({
-            "title": "example",
-            "content": "example",
-            "image": "",
-            "draft": "example",
-            "publish": "2018-02-20",
-            })
-        self.assertTrue(form.is_valid())
-        post = form.save()
-        self.assertTrue(post.title == "example")
-        self.assertTrue(post.content == "example")
-
-    def test_blog_views(self):
-        self.user = User.objects.get(username="test")
-        self.assertTrue(self.client.login(username='test', password='test'))
         url = '/blogcomments/1000/'
         response = self.client.get(url, **{'HTTP_REFERER': url})
         self.assertTrue(response.status_code == 404,
                         msg="Got code %s on %s" % (response.status_code, url))
-        self.post = Post.objects.create(user=self.user, title="example", slug="example", content="example",
+        self.post = Post.objects.create(user=self.user, title="example5", slug="example5", content="example5",
                                         publish="2018-10-08")
         self.assertTrue(self.client.login(username='test', password='test'))
-        url = '/news/example/'
+        url = '/news/example5/'
         response = self.client.post(url, {})
         self.assertTrue(response.status_code == 302 or response.status_code == 200,
                         msg="Got code %s on %s" % (response.status_code, url))
         self.client.logout()
         response = self.client.get(url, **{'HTTP_REFERER': url})
         self.assertTrue(response.status_code == 404, msg="Got code %s on %s" % (response.status_code, url))
+
+        form = PostForm({
+            "title": "formtest",
+            "content": "formtest",
+            "image": "",
+            "draft": "formtest",
+            "publish": "2018-02-20",
+            })
+        self.assertTrue(form.is_valid())
+        post = form.save()
+        self.assertTrue(post.title == "formtest")
+        self.assertTrue(post.content == "formtest")
